f20de0f0f68eb186eb998a721b05f1520647ff5c
Alex Holliday <ajhollid@gmail.com>
2018-04-25 20:27:54 +0000

    Implemented new and create

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -17,4 +17,11 @@ class Admin::CategoriesController < ApplicationController
             render :new
         end
     end
+
+    private
+    def category_params
+        params.require(:category).permit(
+          :name
+        )
+      end
 end
