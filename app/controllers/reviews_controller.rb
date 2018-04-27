cba2e4d657022385d25715f6a100798f08698aa5
Alex <ajollid@gmail.com>
2018-04-27 11:55:11 -0700

    cleaned up reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,27 @@
 class ReviewsController < ApplicationController
+    before_action :set_product, only: [:create]
 
-    def create
-        @product = Product.find(params[:product_id])
+    
+    def create        
          @review = @product.reviews.new({
             user: current_user,
             description: params['description'],
             rating: params['rating']['rating_value']
         })
 
+
         if(@review.save)
             redirect_to root_path
         else
-            redirect_to :back
+            
+             render json: @review.errors, status: :unprocessable_entity 
+            #redirect_to :back
         end
 
     end
+
+    private
+        def set_product
+            @product = Product.find(params[:product_id])
+        end
 end
